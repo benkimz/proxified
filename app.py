d56cdabf6141134b728bf09ce8af4284155ae3c7
benkimz <benkim3619@gmail.com>
2024-11-06 19:57:52 +0300

    Expose port 8000

--- app.py
+++ app.py
@@ -59,4 +59,4 @@ class ProxyServerApp(ShadowServer):
 app = ProxyServerApp(settings_file="settings.json")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000)
+    app.run(host="0.0.0.0", port=8000)
